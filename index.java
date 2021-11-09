107c33e394223e8d1979b0d3c9f221548928d562
LLelouchfer <81318955+LLelocuhfer@users.noreply.github.com>
2021-11-09 14:44:50 -0600

    Delete index.java

--- index.java
+++
@@ -1,8 +0,0 @@
-public class TablaMultiplicar{
-    public static void main(String arg[]){
-        int numero = Integer.parseInt(arg[0]);
-        for(int i = 1 ; i<=10 ; i++){
-            System.out.println(""+numero+" * "+i+" = "+(i*numero));
-        }
-    }
-}
