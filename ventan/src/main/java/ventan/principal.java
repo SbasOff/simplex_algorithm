1f7fbaa0b7a40826fd36d5d738ade463abb5b1af
Jesus Mendez <92119524+Jedez@users.noreply.github.com>
2021-11-09 15:08:54 -0600

    Update principal.java

--- ventan/src/main/java/ventan/principal.java
+++ ventan/src/main/java/ventan/principal.java
@@ -4,6 +4,9 @@ public class principal {
     public static void main(String args[]){
         ventana v = new ventana();
         v.setVisible(true);// hacemos que la vetana sea visible 
+        
+        
+        
     } 
            
 }
